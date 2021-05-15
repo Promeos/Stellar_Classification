03e16e4fa5b2bfe4608b34a8801a90ce997e9a71
promeos <christopher.logan.ortiz@gmail.com>
2021-05-15 05:36:49 -0500

    Transfer explore visualizations to explore.py file, formatting categorical charts for simplicity

--- src/explore.py
+++ src/explore.py
@@ -1,3 +1,275 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def pearson_correlations(train):
+    '''
+    Create a pearson correlation heatmap with correlated values less than .5 removed and 
+    greater than -.5 removed.
+    '''
+    sns.set_context('talk')
+
+    # Calculate the correlations between the numeric features and remove the original columns
+    correlations = train.corr().iloc[4:, 4:]
+    correlations = correlations[((correlations < -0.5)|(correlations > 0.5))]
+
+    # Create a boolean array to use as a mask to remove the upper corner from the heatmap.
+    mask = np.zeros_like(correlations)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set the context of the heatmap with a white background
+    with sns.axes_style("white"):
+        
+        f, ax = plt.subplots(figsize=(20, 16))
+
+        ax.set_title("Absolute Magnitude, Luminosity, and Radius correlate with Star Type", fontsize=28)
+
+
+        ax = sns.heatmap(correlations,
+                         annot=True,
+                         cmap='coolwarm',
+                         vmin=-1,
+                         vmax=1,
+                         mask=mask,
+                         fmt='.03f')
+        plt.show()
+
+
+def scaled_pairplot(train):
+    '''
+    Create a pairplot using features scaled with the MinMaxScaler. A KDE overlay is added to the
+    pairplot to show overlapping groups and distinct groups.
+    '''
+    sns.set_context('notebook')
+
+    pairs1 = sns.pairplot(data=train,
+                          hue='star_type_name',
+                          vars=['spectral_class_num',
+                                'color_num',
+                                'scaled_temperature',
+                                'scaled_luminosity',
+                                'scaled_radius',
+                                'scaled_absolute_magnitude'],
+                          palette='Set1',
+                          corner=True)
+
+    pairs1.map_lower(sns.kdeplot, levels=2, color=".2")
+
+    pairs1.fig.suptitle("Absolute Magnitude and Temperature contain Star Type Clusters",
+                        fontsize=26,
+                        y=1.03)
+
+    plt.show()
+
+
+def quantiled_pairplot(train):
+    '''
+    Create a pairplot using quantile scaled features.
+    '''
+    sns.set_context('notebook')
+
+    pairs2 = sns.pairplot(data=train,
+                          hue='star_type_name',
+                          vars=['quantiled_temperature',
+                                'quantiled_luminosity',
+                                'quantiled_radius',
+                                'quantiled_absolute_magnitude'],
+                          palette='Set1',
+                          corner=True)
+
+    pairs2.fig.suptitle("Quantiled Radius and Quantiled Luminosity contain Star Type Clusters",
+                        fontsize=18,
+                        y=1.05)
+
+    plt.show()
+
+
+def three_d_scatter(train):
+    '''
+    Create a 3-D scatterplot using quantiled_radius, scaled_absolute_radius,
+    and quantiled luminosity.
+    '''
+    # Plot Formatters
+    star_names = ['Hyper Giants', 'Super Giants', 'Main Sequence', 'White Dwarf', 'Brown Dwarf', 'Red Dwarf']
+    markers = ['*','X','P','h','p','D']
+    marker_color = ['blue', 'lightblue', 'gold', 'whitesmoke', 'saddlebrown', 'red']
+
+    sns.set_context('talk')
+
+    # Create the 3d canvas
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(projection='3d')
+
+    # Visualize each star type with its assigned marker and color
+    for star, marker, color in zip(star_names, markers, marker_color):
+        temp = train.loc[train['star_type_name'] == star]
+        
+        ax.scatter(xs=temp["quantiled_radius"],
+                   ys=temp["scaled_absolute_magnitude"],
+                   zs=temp["quantiled_luminosity"],
+                   c=color,
+                   s=125,
+                   alpha=.75,
+                   marker=marker,
+                   ec='black')
+
+    # Label Formatting
+    ax.set(title='Brightness and Size are Distinct for Each Star Type',
+           xlabel='Radius (Quantiled)',
+           ylabel='Absolute Magnitude (Scaled)',
+           zlabel='Luminosity (Quantiled)')
+
+    ax.view_init(45,45)
+
+    plt.tight_layout()
+    plt.legend(star_names)
+    plt.show()
+
+
+
+def two_d_scatter(train):
+    '''
+    Create a 2-D scatterplot using quantiled_radius, and scaled_absolute_radius.
+    '''
+    # Plot formatters
+    star_names = ['Hyper Giants', 'Super Giants', 'Main Sequence', 'White Dwarf', 'Brown Dwarf', 'Red Dwarf']
+    markers = ['*','X','P','h','p','D']
+    marker_color = ['blue', 'lightblue', 'gold', 'whitesmoke', 'saddlebrown', 'red']
+
+    sns.set_context('talk')
+
+    # Create the 3d canvas
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(projection='3d')
+
+    # Visualize each star type with its assigned marker and color
+    for star, marker, color in zip(star_names, markers, marker_color):
+        temp = train.loc[train['star_type_name'] == star]
+        
+        ax.scatter(xs=temp["quantiled_radius"],
+                   ys=temp["scaled_absolute_magnitude"],
+                   zs=temp["quantiled_luminosity"],
+                   c=color,
+                   s=100,
+                   alpha=.75,
+                   marker=marker,
+                   ec='black')
+    
+    # Label Formatting
+    ax.set(title='Brightness and Size are Distinct for Each Star Type: Top View',
+           xlabel='Radius (Quantiled)',
+           ylabel='Absolute Magnitude (Scaled)',
+           zlabel='')
+
+    ax.view_init(90,0)
+
+    plt.legend(star_names)
+    plt.show()
+
+
+def boxplots(train):
+    '''
+    Create boxplots for each star type and feature.
+    '''
+    # Create a list of numeric columns to visualize
+    boxplot_column_names = list(train.select_dtypes(include='number').columns)
+    boxplot_column_names.remove('star_type')
+    boxplot_column_names.remove('spectral_class_num')
+    boxplot_column_names.remove('color_num')
+
+    # Create a variable to store the name of the target variable.
+    TARGET = 'star_type'
+
+    # Format names to substitute for plot labels
+    fmt_column_names = [column.replace('_', ' ').title() for column in boxplot_column_names]
+    fmt_target_name = TARGET.replace('_', ' ').title()
+
+    sns.set_context('talk')
+
+    # Create the plotting area with 12 subplots split into 6 rows x 2 columns
+    fig, axs = plt.subplots(6, 2, figsize=(24, 36))
+
+    # Ravel the axis into a list to interate over
+    axs = axs.ravel()
+
+    with sns.axes_style("white"):
+        plt.style.use('tableau-colorblind10')
+
+        # Create a boxplot of each feature for all stars
+        for i, (col_name, fmt_col_name) in enumerate(zip(boxplot_column_names, fmt_column_names)):
+            sns.boxplot(ax=axs[i],
+                        x=train['star_type_name'],
+                        y=train[col_name],
+                        whis=3)
+
+            axs[i].set(title=f'Distribution of {fmt_col_name}',
+                       xlabel='',
+                       ylabel=fmt_col_name)
+
+        plt.tight_layout()
+
+    plt.subplots_adjust(hspace=.5)
+    plt.show()
+
+
+def startype_by_color(train):
+    '''
+    Create a catego
+    '''
+    star_color_ctb = pd.crosstab(train['star_type_name'], train['color'])
+
+    plt.figure(figsize=(10, 8))
+    cbar_format = {'ticks':list(np.arange(0, 21, 5))}
+
+    sns.heatmap(star_color_ctb,
+                cmap='Greens',
+                annot=True,
+                cbar_kws=cbar_format)
+
+    plt.title('Count of Stars by Type and Color', fontsize=22)
+    plt.xlabel('')
+    plt.ylabel('')
+
+    plt.xticks(ticks=np.arange(.5, len(star_color_ctb.columns)+.5),
+            labels=[label.title() for label in list(star_color_ctb.columns)],
+            ha='right',
+            rotation=45)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def startype_by_spectral_class(train):
+    '''
+
+    '''
+    star_spectral_class_ctb = pd.crosstab(train['star_type_name'], train['spectral_class'])
+
+    plt.figure(figsize=(8, 6))
+    cbar_format = {'ticks':list(np.arange(0, 21, 5))}
+
+    sns.heatmap(star_spectral_class_ctb,
+                cmap='Greens',
+                annot=True,
+                cbar_kws=cbar_format)
+
+    plt.title('Count of Stars by Type and Spectral Class', fontsize=18)
+    plt.xlabel('')
+    plt.ylabel('')
+
+    plt.show()
+
+
+def color_by_spectral_class(train):
+    '''
+
+    '''
+    color_spectral_class_ctb = pd.crosstab(train['color'], train['spectral_class'])
+
+
 # sns.set_context('talk')
 
 # fig, axs = plt.subplots(6, 2, figsize=(24, 36))
--- src/preprocess.py
+++ src/preprocess.py
@@ -7,25 +7,18 @@ from sklearn.preprocessing import MinMaxScaler, QuantileTransformer
 def create_datasets(dataset):
     '''
     Splits the stars dataset into train, validate, and test sets.
-
     There are 40 observations for each of the 6 star types
-    Training set contains 50% of the dataset
     
-    -37.5%-12.5%
+    Data splits = 50%-37.5%-12.5% | 20, 15, 5
     
- 
-
     Parameters
     ----------
     dataset :
+        The stars dataset returned by the `get_star_data` function
     
     Returns
     -------
-    train
-
-    validate
-
-    test
+    train, validate, test
 
     '''
     # Split the dataset into train, and validation + test sets
@@ -51,20 +44,11 @@ def create_X_y_sets(train, validate, test):
 
     Parameters
     ----------
-    train :
-
-    validate :
-
-    test : 
+    train, validate, test
     
     Returns
     -------
-    X_train
-    y_train
-    X_validate
-    y_validate
-    X_test
-    y_test
+    X_train, y_train, X_validate, y_validate, X_test, y_test
     '''
     # Create a variable to store the target name to the list of columns dropped from
     # the X datasets
@@ -84,8 +68,10 @@ def create_X_y_sets(train, validate, test):
 
 def arrange_columns(train, validate, test):
     '''
-
+    Rearrange the column names of each dataset to place the target variables
+    as the last columns.
     '''
+    # Variable to store ordered column names
     arranged_cols = ['temperature',
     'luminosity',
     'radius',
@@ -105,6 +91,7 @@ def arrange_columns(train, validate, test):
     'star_type_name',
     'star_type']
     
+    # Reorder the columns in the train, validate, and test sets.
     train = train[arranged_cols]
     validate = validate[arranged_cols]
     test = test[arranged_cols]
@@ -114,21 +101,29 @@ def arrange_columns(train, validate, test):
 
 def scale_data(train, validate, test):
     '''
-
+    Scale the temperature, luminosity, radius, and absolute columns using a MinMaxScaler
+    and adds them to the train, validate, and test datasets. These scaled columns are
+    passed into a Quantile Transformer to create normally distributed data.
     '''
     cols_to_scale = ['temperature', 'luminosity', 'radius', 'absolute_magnitude']
+
+    # Create a list of new scaled column names
     scaled_cols = [f'scaled_{col}' for col in cols_to_scale]
     quantile_cols = [f'quantiled_{col}' for col in cols_to_scale]
 
 
     def min_max_scaler(train, validate, test):
         '''
-
+        Scales the train, validate, and test datasets using a MinMaxScaler and
+        returns values between 0 and 1.
         '''
+        # Grab the variables initialized above
         nonlocal cols_to_scale, scaled_cols
 
+        # Create the Scaler Object
         scaler = MinMaxScaler()
 
+        # Fit the scaler to the training set and transform the train, validate, and test sets.
         train[scaled_cols] = scaler.fit_transform(train[cols_to_scale])
         validate[scaled_cols] = scaler.transform(validate[cols_to_scale])
         test[scaled_cols] = scaler.transform(test[cols_to_scale])
@@ -136,54 +131,27 @@ def scale_data(train, validate, test):
         return train, validate, test
 
 
-
     def quantile_scaler(train, validate, test):
         '''
-
+        Scales the MinMaxScaled features (Temperature, luminosity, radius, absolute_magnitude) with
+        a Quantile Transformer scale to create normally distributed data.
         '''
+        # Grab the variables intialized above
         nonlocal cols_to_scale, quantile_cols
 
+        # Create a Scaler object with 20 quanitle bins following a normal distribution
         q_scaler = QuantileTransformer(20, output_distribution='normal')
 
+        # Fit the scaler to the training set and transform the train, validate, and test sets.
         train[quantile_cols] = q_scaler.fit_transform(train[cols_to_scale])
         validate[quantile_cols] = q_scaler.transform(validate[cols_to_scale])
         test[quantile_cols] = q_scaler.transform(test[cols_to_scale])
 
         return train, validate, test
 
-
+    # Scale and arrange the columns in the train, validate, and test sets.
     train, validate, test = min_max_scaler(train, validate, test)
     train, validate, test = quantile_scaler(train, validate, test)
     train, validate, test = arrange_columns(train, validate, test)
-
-    return train, validate, test
-
-
-def arrange_columns(train, validate, test):
-    '''
-
-    '''
-    arranged_cols = ['temperature',
-    'luminosity',
-    'radius',
-    'absolute_magnitude',
-    'color',
-    'spectral_class',
-    'spectral_class_num',
-    'color_num',
-    'scaled_temperature',
-    'scaled_luminosity',
-    'scaled_radius',
-    'scaled_absolute_magnitude',
-    'quantiled_temperature',
-    'quantiled_luminosity',
-    'quantiled_radius',
-    'quantiled_absolute_magnitude',
-    'star_type_name',
-    'star_type']
-    
-    train = train[arranged_cols]
-    validate = validate[arranged_cols]
-    test = test[arranged_cols]
 
     return train, validate, test
